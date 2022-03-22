a0b0a4feb7c18c58c5afc76d51948c6eed87304a
GauravKhedkar <gauravkhedkar2000@gmail.com>
2022-03-21 22:24:01 -0700

    First Java Project

---
+++ src/com/tcs/demo/Demo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.tcs.demo;
+
+/**
+ * @author springuser04
+ *
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Associate Java Engineer");
+	}
+
+}
